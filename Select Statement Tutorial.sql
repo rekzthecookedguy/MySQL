991ce9d4ed19766218ae802b0f0eca9a319b6632
CHEF <reksalinggaw@gmail.com>
2026-02-14 02:56:09 +0700

    Add files via upload

---
+++ Select Statement Tutorial.sql
@@ -0,0 +1,18 @@
+select *
+from parks_and_recreation.employee_demographics;
+
+
+select first_name, 
+last_name, 
+birth_date, 
+age, 
+(age + 9) * 9
+from parks_and_recreation.employee_demographics;
+
+
+select distinct first_name, gender
+from parks_and_recreation.employee_demographics;
+
+
+
+
